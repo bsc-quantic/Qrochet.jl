97b4da0d968c71c5d8e21db0f9d853c3d83fad5a
Todorbsc <todor.krasimirov@bsc.es>
2024-02-15 16:32:48 +0100

    Integrate Quac in Qrochet

---
+++ ext/QrochetQuacExt.jl
@@ -0,0 +1,42 @@
+module QrochetQuacExt
+
+using Qrochet
+using Tenet
+using Quac: Circuit, lanes, arraytype, Swap
+
+function Qrochet.Quantum(circuit::Circuit)
+    n = lanes(circuit)
+    wire = [[Tenet.letter(i)] for i in 1:n]
+    tensors = Tensor[]
+
+    i = n + 1
+    for gate in circuit
+        G = arraytype(gate)
+        array = G(gate)
+
+        if gate isa Swap
+            (a, b) = lanes(gate)
+            wire[a], wire[b] = wire[b], wire[a]
+            continue
+        end
+
+        inds = map(lanes(gate)) do l
+            from, to = last(wire[l]), Tenet.letter(i)
+            i += 1
+            push!(wire[l], to)
+            (from, to)
+        end |> x -> zip(x...) |> Iterators.flatten |> collect
+
+        tensor = Tensor(array, tuple(inds...))
+        push!(tensors, tensor)
+    end
+
+    sites = merge(
+        Dict([Site(site; dual=true) => first(index) for (site, index) in enumerate(wire)]),
+        Dict([Site(site; dual=false) => last(index) for (site, index) in enumerate(wire)])
+    )
+    
+    Quantum(Tenet.TensorNetwork(tensors), sites)
+end
+
+end

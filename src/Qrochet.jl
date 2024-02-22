cf0ac4c8e8fd75aaa73ab220ab582cde9a53da22
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-02-22 18:58:42 +0100

    Export `truncate!`, `isleftcanonical`, `isrightcanonical`

--- src/Qrochet.jl
+++ src/Qrochet.jl
@@ -17,8 +17,8 @@ export Product
 include("Ansatz/Chain.jl")
 export Chain
 export MPS, pMPS, MPO, pMPO
-export leftindex, rightindex, canonize_site, canonize_site!
-export mixed_canonize, mixed_canonize!
+export leftindex, rightindex, canonize_site, canonize_site!, truncate!
+export mixed_canonize, mixed_canonize!, isleftcanonical, isrightcanonical
 
 # reexports from Tenet
 using Tenet

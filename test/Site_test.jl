5be44358f8fc56bb2873f08439188c7af209da91
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-03-24 23:05:01 +0100

    Refactor `Site` to N-dimensional coordinates

--- test/Site_test.jl
+++ test/Site_test.jl
@@ -1,25 +1,63 @@
 @testset "Site" begin
+    using Qrochet: id
+
     s = Site(1)
-    @test s.id == 1
-    @test s.dual == false
+    @test id(s) == 1
+    @test CartesianIndex(s) == CartesianIndex(1)
+    @test isdual(s) == false
 
     s = Site(1; dual = true)
-    @test s.id == 1
-    @test s.dual == true
+    @test id(s) == 1
+    @test CartesianIndex(s) == CartesianIndex(1)
+    @test isdual(s) == true
+
+    s = Site(1, 2)
+    @test id(s) == (1, 2)
+    @test CartesianIndex(s) == CartesianIndex((1, 2))
+    @test isdual(s) == false
+
+    s = Site(1, 2; dual = true)
+    @test id(s) == (1, 2)
+    @test CartesianIndex(s) == CartesianIndex((1, 2))
+    @test isdual(s) == true
 
     s = site"1"
-    @test s.id == 1
-    @test s.dual == false
+    @test id(s) == 1
+    @test CartesianIndex(s) == CartesianIndex(1)
+    @test isdual(s) == false
 
     s = site"1'"
-    @test s.id == 1
-    @test s.dual == true
+    @test id(s) == 1
+    @test CartesianIndex(s) == CartesianIndex(1)
+    @test isdual(s) == true
+
+    s = site"1,2"
+    @test id(s) == (1, 2)
+    @test CartesianIndex(s) == CartesianIndex((1, 2))
+    @test isdual(s) == false
+
+    s = site"1,2'"
+    @test id(s) == (1, 2)
+    @test CartesianIndex(s) == CartesianIndex((1, 2))
+    @test isdual(s) == true
 
     s = site"1" |> adjoint
-    @test s.id == 1
-    @test s.dual == true
+    @test id(s) == 1
+    @test CartesianIndex(s) == CartesianIndex(1)
+    @test isdual(s) == true
 
     s = site"1'" |> adjoint
-    @test s.id == 1
-    @test s.dual == false
+    @test id(s) == 1
+    @test CartesianIndex(s) == CartesianIndex(1)
+    @test isdual(s) == false
+
+    s = site"1,2" |> adjoint
+    @test id(s) == (1, 2)
+    @test CartesianIndex(s) == CartesianIndex((1, 2))
+    @test isdual(s) == true
+
+    s = site"1,2'" |> adjoint
+    @test id(s) == (1, 2)
+    @test CartesianIndex(s) == CartesianIndex((1, 2))
+    @test isdual(s) == false
 end

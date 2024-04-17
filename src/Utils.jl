98549d2149fed9cff8f1c483f219a5391949fb0a
Jofre Vallès <jofrevalles99@gmail.com>
2024-04-17 21:09:39 +0200

    Reset __indexcounter if we pass the limit

--- src/Utils.jl
+++ src/Utils.jl
@@ -3,5 +3,5 @@ using Tenet
 const __indexcounter::Threads.Atomic{Int} = Threads.Atomic{Int}(1)
 
 currindex() = Tenet.letter(__indexcounter[])
-nextindex() = Tenet.letter(Threads.atomic_add!(__indexcounter, 1))
+nextindex() = (__indexcounter.value >= 135000) ? resetindex() : Tenet.letter(Threads.atomic_add!(__indexcounter, 1))
 resetindex() = Tenet.letter(Threads.atomic_xchg!(__indexcounter, 1))

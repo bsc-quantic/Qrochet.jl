c1eab27696ab2205bcfc2e9ed10715112d2872a3
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-04-17 16:57:18 +0200

    Add `resetindex` utility for resetting index counter

--- src/Utils.jl
+++ src/Utils.jl
@@ -4,3 +4,4 @@ const __indexcounter::Threads.Atomic{Int} = Threads.Atomic{Int}(1)
 
 currindex() = Tenet.letter(__indexcounter[])
 nextindex() = Tenet.letter(Threads.atomic_add!(__indexcounter, 1))
+resetindex() = Tenet.letter(Threads.atomic_xchg!(__indexcounter, 1))

266ae528e6f9084c2a41720a6fa8e9f96b1d7f96
Sergio Sánchez Ramírez <sergio.sancnchez.ramirez+git@bsc.es>
2024-06-25 18:41:38 +0200

    Fix Adapt integration

--- ext/QrochetAdaptExt.jl
+++ ext/QrochetAdaptExt.jl
@@ -5,5 +5,7 @@ using Tenet
 using Adapt
 
 Adapt.adapt_structure(to, x::Quantum) = Quantum(adapt(to, TensorNetwork(x)), x.sites)
+Adapt.adapt_structure(to, x::Product) = Product(adapt(to, Quantum(x)))
+Adapt.adapt_structure(to, x::Chain) = Chain(adapt(to, Quantum(x)), boundary(x))
 
 end
